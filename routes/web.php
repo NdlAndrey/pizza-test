202baa51f0207d13263dc230b3120a305fd385cf
Andrew <nedelcev.a.g@gmail.com>
2020-06-11 13:43:31 +0300

    created new controller for user authentication and change route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function () {
+    Route::get('/', 'AdminController@dashboard')->name('dashboard');
+});
