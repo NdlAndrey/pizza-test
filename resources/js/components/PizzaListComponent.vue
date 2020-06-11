99aa3892e995f26501dfb6044a6fd903076ef126
Andrew <nedelcev.a.g@gmail.com>
2020-06-11 17:30:32 +0300

    added in component PizzaList modal for create and update pizza

--- resources/js/components/PizzaListComponent.vue
+++ resources/js/components/PizzaListComponent.vue
@@ -3,7 +3,7 @@
     <div class="card-header">
       Pizza List
 
-      <button class="btn btn-primary btn-sm float-right" @click="">Create</button>
+      <button class="btn btn-primary btn-sm float-right" @click="modalOpen">Create</button>
     </div>
     <div class="card-body">
       <table class="table">
@@ -25,20 +25,65 @@
           <td>{{ item.price }}</td>
           <td>{{ item.date }}</td>
           <td>
-            <button type="button" class="btn btn-primary btn-sm">Edit</button>
-            <button type="button" class="btn btn-danger btn-sm" @click="handlerDelete(item.route_delete)">Delete</button>
+            <button type="button" class="btn btn-primary btn-sm" @click="modalOpen(item.id)">Edit</button>
+            <button type="button" class="btn btn-danger btn-sm" @click="handlerDelete(item.route_delete)">Delete
+            </button>
           </td>
         </tr>
         </tbody>
       </table>
     </div>
+
+    <!-- Vertically centered modal -->
+    <b-modal v-model="modalShow"
+             :title="modalTitle"
+             @ok="handleOk"
+             button-size="sm">
+      <form>
+        <div class="form-group">
+          <label for="form-name" class="col-form-label">Name:</label>
+          <input type="text" class="form-control" id="form-name" v-model="form.name">
+        </div>
+
+        <p class="text-center">Ingredients</p>
+        <hr>
+        <div class="form-group" v-for="(ingredient, index) in form.ingredients">
+          <div class="row">
+            <div class="col-md-7">
+              <label :for="`form-name-${index}`" class="col-form-label col-form-label-sm">Name:</label>
+              <select class="custom-select custom-select-sm" v-model="ingredient.ingredient_id">
+                <option selected>Select ingredient</option>
+              </select>
+
+            </div>
+            <div class="col-md-3">
+              <label :for="`form-sort-${index}`" class="col-form-label col-form-label-sm">Sort:</label>
+              <input type="text" class="form-control form-control-sm" :id="`form-sort-${index}`" v-model="ingredient.sort">
+            </div>
+            <div class="col-md-2 d-flex align-items-end">
+              <div class="btn-group mr-2" role="group" aria-label="First group">
+                <button type="button" class="btn btn-secondary btn-sm" @click="addOption">+</button>
+                <button type="button" class="btn btn-danger btn-sm" :disabled="index === 0" @click="deleteOption(index)">-</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </b-modal>
   </div>
 
 </template>
 
 <script>
+  import { BButton, BModal, VBModal } from "bootstrap-vue";
+
   export default {
     name: "PizzaListComponent",
+
+    components: {
+      BButton,
+      BModal
+    },
 
     props: {
       source: {
@@ -49,32 +94,71 @@
 
     data() {
       return {
-        items: []
-      }
+        items: [],
+
+        modalTitle: 'Create Pizza',
+        modalShow: false,
+        form: {
+          name: '',
+          ingredients: []
+        }
+      };
     },
 
     mounted() {
-      this.handlerInit()
+      this.handlerInit();
     },
 
+
     methods: {
+      // Method get table data
       async handlerInit() {
         const {data} = await axios.get(this.source);
 
         this.items = data.data;
       },
 
+      // Method delete item by table
       async handlerDelete(source) {
         const {data} = await axios.delete(source);
 
         let index = this.items.findIndex(item => item.id === data.id);
         this.items.splice(index, 1);
+      },
+
+      // Method open modal and set values to form
+      modalOpen(id = null) {
+        let index = this.items.findIndex(item => item.id === id);
+        this.modalTitle = index !== -1 ? 'Edit Pizza' : 'Create Pizza';
+
+        this.form = {
+          name: index !== -1 ? this.items[index].name : '',
+          ingredients:  index !== -1 ? this.items[index].ingredients : [{ingredient_id: null, sort: 0}],
+        }
+
+        this.modalShow = true;
+      },
+
+      // Method add row for ingredients
+      addOption() {
+        this.form.ingredients.push({ingredient_id: null, sort: 0});
+      },
+
+      // Method delete ingredient
+      deleteOption(index) {
+        this.form.ingredients.splice(index, 1);
+      },
+
+      handleOk() {
+        console.log(1);
       }
     }
 
   };
 </script>
 
-<style scoped>
-
+<style>
+  .modal-backdrop {
+    opacity: .5!important;
+  }
 </style>
