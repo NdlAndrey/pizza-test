5029b7a7d4bdbd7ad4c258a2cdafb9cdf8a289e3
Andrew <nedelcev.a.g@gmail.com>
2020-06-11 15:36:30 +0300

    make a route and Controller for Crud Pizza

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,10 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function () {
     Route::get('/', 'AdminController@dashboard')->name('dashboard');
+
+    // ajax get list pizza
+    Route::get('get-pizza', 'PizzaController@getPizza')->name('pizza.get-ajax');
+
+    // resource pages pizza
+    Route::resource('pizza', 'PizzaController');
 });
